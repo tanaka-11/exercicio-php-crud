23b7074fb8444cdddff2098ed6b594a8cf19813f
Marina Tanaka <huttenloucher@gmail.com>
2023-08-31 18:26:49 -0300

    update

--- index.php
+++ index.php
@@ -1,21 +1,35 @@
 <!DOCTYPE html>
+
 <html lang="pt-br">
+
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício CRUD com PHP e MySQL</title>
+
+<title>Sistema de Médias</title>
+
 <link href="css/style.css" rel="stylesheet">
+
 </head>
 <body>
 
 <div class="containerIndex text-center">
-    <h1>Sistema Média de Alunos</h1>
+    <h1>Sistema de Média dos Alunos</h1>
 
-    <h2>Gerenciamento de alunos, notas, médias e aprovação/reprovação</h2>
+    <h2>Gerenciamento de alunos, notas, médias e situação</h2>
+
+    <br>
     
     <ul>
-        <li><a href="visualizar.php">Visualizar Alunos</a></li>
-        <li><a href="inserir.php">Inserir novo aluno</a></li>
+        <li>
+            <a href="visualizar.php">Visualizar Alunos</a>
+        </li>
+
+        <br>
+
+        <li>
+            <a href="inserir.php">Inserir novo aluno</a>
+        </li>
     </ul>
 </div>
 
