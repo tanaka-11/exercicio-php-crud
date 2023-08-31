8747c41fe617c0227d08e074eeaccd18564a2810
Marina Tanaka <huttenloucher@gmail.com>
2023-08-31 19:18:52 -0300

    update

--- visualizar.php
+++ visualizar.php
@@ -4,58 +4,65 @@ $listaDeAlunos = lerAlunos($conexao);
 ?>
 
 <!DOCTYPE html>
+
 <html lang="pt-br">
+
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>Lista de alunos - Visualização</title>
+<title>Lista de Alunos - Visualização</title>
 
 <link href="css/style.css" rel="stylesheet">
 </head>
 
 <body>
 
-<div class="containerVisualizar text-center">
-    <h1>Lista de alunos</h1>
-    <hr>
-    <p><a href="inserir.php">Inserir novo aluno</a></p>
-</div>
+    <div class="containerVisualizar text-center">
+        <h1>Lista de Alunos</h1>
+    
+        <p><a href="inserir.php">Inserir novo aluno</a></p>
+    </div>
 
     <div class="alunos">
 <?php foreach($listaDeAlunos as $aluno) {?>
     <article class="aluno">  
     <p>
-        <b>Nome do Aluno: </b><?=$aluno['nomeAluno']?>
+        <b>Nome do Aluno : </b><?=$aluno['nomeAluno']?>
     </p>
 
     <p>
-        <b>1ª nota: </b><?=$aluno['primeiraNota']?>
+        <b>1ª nota : </b><?=$aluno['primeiraNota']?>
     </p>
 
     <p>
-        <b>2ª nota: </b><?=$aluno['segundaNota']?>
+        <b>2ª nota : </b><?=$aluno['segundaNota']?>
     </p>
 
     <p>
-        <b>Média: </b><?=$aluno['Média']?>
+        <b>Média : </b><?=$aluno['Média']?>
     </p>
 
     <p>
-        <b>Situação: </b>
+        <b>Situação : </b>
     </p>
 
     <br>
 
     <p>
-        <a class="editarAluno" href="">Editar</a>
+        <a class="editarAluno" href="">Editar</a> |
         <a class="excluirAluno" href="">Excluir</a>
     </p>
+
     </article>  
 <?php } ?>
 
-    <p><a href="index.php">Voltar ao início</a></p>
-    </div>
+    <footer>
+        <p>
+            <a href="index.php">Voltar</a>
+        </p>
+    </footer>    
+</div>
 
 
 </body>
